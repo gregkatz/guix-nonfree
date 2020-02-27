4109ca6466c26bf257490d9eebbbbcc749e70ad1
Gregory Katz <gregkatz@gmail.com>
2020-02-27 09:04:15 -0500

    added some imports I think

--- gnu/packages/libre-custom.scm
+++ gnu/packages/libre-custom.scm
@@ -1,3 +1,12 @@
+(define-module (gnu packages linux-nonfree)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages tls)
+  #:use-module (guix build-system trivial)
+  #:use-module (guix git-download)
+  #:use-module (guix packages)
+  #:use-module (guix download))
+
 (define-public libre-custom
   (package
    (inherit linux-libre)
