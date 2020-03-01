0d247501fcf09ae0532d98317d5bc6809dc8e242
Gregory Katz <gregkatz@gmail.com>
2020-02-29 20:38:37 -0500

    fix a syntax error

--- gnu/services/wireguard.scm
+++ gnu/services/wireguard.scm
@@ -1,10 +1,38 @@
-(define-module (mnd services wireguard)
+(define-module (gnu services wireguard)
   #:use-module (gnu services)
   #:use-module (gnu services shepherd)
-  ;;#:use-module (gnu packages wireguard)
   #:use-module (gnu packages screen)
   #:use-module (guix gexp)
   #:use-module (guix records)
+  #:use-module (ice-9 match)
+  #:use-module (gnu services)
+  #:use-module (gnu services base)
+  #:use-module (gnu services shepherd)
+  #:use-module (gnu services dbus)
+  #:use-module (gnu system shadow)
+  #:use-module (gnu system pam)
+  #:use-module (gnu packages admin)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages bash)
+  #:use-module (gnu packages connman)
+  #:use-module (gnu packages freedesktop)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages tor)
+  #:use-module (gnu packages usb-modeswitch)
+  #:use-module (gnu packages messaging)
+  #:use-module (gnu packages networking)
+  #:use-module (gnu packages ntp)
+  #:use-module (gnu packages wicd)
+  #:use-module (gnu packages gnome)
+  #:use-module (guix gexp)
+  #:use-module (guix records)
+  #:use-module (guix modules)
+  #:use-module (guix packages)
+  #:use-module (guix deprecation)
+  #:use-module (rnrs enums)
+  #:use-module (srfi srfi-1)
+  #:use-module (srfi srfi-9)
+  #:use-module (srfi srfi-26)
   #:use-module (ice-9 match)
   #:export (wireguard-service))
 
@@ -17,17 +45,17 @@
   (ruleset wireguard-configuration-ruleset ; file-like object
            ))
 
-define wireguard-shepherd-service
+(define wireguard-shepherd-service
   (match-lambda
-    (($ <wireguard-configuration> package ruleset)
-     (let ((wireguard (file-append package "/bin/wg-quick")))
-       (shepherd-service
-        (documentation "Wireguard VPN service")
-        (provision '(wireguard))
-        (start #~(lambda _
-                   (invoke #$wireguard "up" #$ruleset)))
-        (stop #~(lambda _
-                  (invoke #$wireguard "down" #$ruleset))))))))
+   (($ <wireguard-configuration> package ruleset)
+    (let ((wireguard (file-append package "/bin/wg-quick")))
+      (shepherd-service
+       (documentation "Wireguard VPN service")
+       (provision '(wireguard))
+       (start #~(lambda _
+		  (invoke #$wireguard "up" #$ruleset)))
+       (stop #~(lambda _
+		 (invoke #$wireguard "down" #$ruleset))))))))
 
 (define wireguard-service-type
   (service-type
